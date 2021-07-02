0bf629b05ab103a62c8ff085a33e16356e4979ad
educartj <educartj@gmail.com>
2021-07-02 02:58:19 -0300

    Segunda aula

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
+import "reflect-metadata";
 import express from "express";
+
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
-app.get("/test", (req, res) => {
-  return res.send("ola");
-})
-app.post("/test-post", (req, res) => {
-  return res.send("olá NLW método post");
-})
+app.use(express.json());
+
+app.use(router);
+
 app.listen(3000, () => console.log("Server is running NLW"));
